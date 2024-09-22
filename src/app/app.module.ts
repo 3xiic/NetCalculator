3abd5ffb09aec90b4e1b1c579f8546dd004a821e
3xiic <122654327+3xiic@users.noreply.github.com>
2024-09-22 02:27:57 -0500

    First upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { IpService } from './ip.service';
+import { IpCalcComponent } from './ip-calc/ip-calc.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IpCalcComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
